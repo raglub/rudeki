869987b7292df4eadf0a4f998b818fccc957f31e
Michał Szyma <raglub@gmail.com>
2012-05-10 23:27:23 +0200

    update class config and initialize alias_method

--- lib/rudeki.rb
+++ lib/rudeki.rb
@@ -1,14 +1,19 @@
 # encoding: utf-8
+require "rudeki/config"
 
-alias :old_puts :puts
+Rudeki::Config.methods.each do |method|
+  Kernel::send(:alias_method, "old_#{method}".intern, "#{method}".intern)
+  # TODO
+end
 
 def puts(arg)
+  length = 80
   result = " puts -> "+ caller.first.to_s
-  old_puts("╔" + "═"*(result.size+6) + "╗")
-  old_puts("║" + "▒"*(result.size+6)+ "║")
-  old_puts("║" + "▒▒ #{result} ▒▒"+ "║")
-  old_puts("║" + "▒"*(result.size+6)+ "║")
+  old_puts("╔" + "═"*length)
+  old_puts("║" + "▒"*length)
+  old_puts("#{result}")
+  old_puts("║" + "▒"*length)
   old_puts(arg)
-  old_puts("║" + "▒"*(result.size+6)+ "║")
-  old_puts("╚" + "═"*(result.size+6)+ "╝")
+  old_puts("║" + "▒"*length)
+  old_puts("╚" + "═"*length)
 end
