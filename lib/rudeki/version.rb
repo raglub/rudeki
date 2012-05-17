05c4f12a9baee399321fab3f67e38f38bcff0829
Michał Szyma <raglub@gmail.com>
2012-05-17 23:19:27 +0200

    Configuration works properly for methods p, puts and class StandErrors

--- lib/rudeki/version.rb
+++ lib/rudeki/version.rb
@@ -1,3 +1,3 @@
 module Rudeki
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
--- spec/rudeki_spec.rb
+++ spec/rudeki_spec.rb
@@ -1,8 +1,14 @@
 # encoding: utf-8
 require 'rudeki'
 
-describe "Rudeki" do
-  it "putter" do
+Rudeki::Config.set do |conf|
+  conf.methods = [:p]
+end
+
+#STDIN.readline(puts "3213")
+#STDOUT.print
+describe Rudeki do
+  it "should properly show frames of method puts" do
     puts 123
   end
 end
